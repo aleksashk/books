0cec8fb51a096507c5d5b3eeb0a33f50907b7c27
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-11-02 14:15:33 +0300

    Initial commit

---
+++ src/test/java/by/flameksandr/books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package by.flameksandr.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
