748d6b04c9d051c49307d86932a5e366aeb1cb3c
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-11-02 17:35:41 +0300

    added BookRepository

---
+++ src/main/java/by/flameksandr/books/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package by.flameksandr.books.repositories;
+
+import by.flameksandr.books.domain.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<BookEntity, String> {
+}
